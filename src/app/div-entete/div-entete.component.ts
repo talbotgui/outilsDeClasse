ee395667048e3306e73d86e126a06d80d9eead3e
Guillaume TALBOT <talbotgui@gmail.com>
2024-11-05 19:20:32 +0100

    Amélioration de l'impression pour intégrer l'entête paramétré dans les données chargées

--- src/app/div-entete/div-entete.component.ts
+++ src/app/div-entete/div-entete.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractComponent } from '../directives/abstract.component';
+import { ContexteService } from '../service/contexte-service';
+import { tap } from 'rxjs';
+import { HtmlPipe } from '../pipes/html.pipe';
 
 @Component({
     selector: '[div-entete]', templateUrl: './div-entete.component.html', styleUrl: './div-entete.component.scss',
-    standalone: true, imports: []
+    standalone: true, imports: [
+        // Pipes
+        HtmlPipe
+    ]
 })
-export class DivEnteteComponent {
+export class DivEnteteComponent  extends AbstractComponent implements OnInit {
+
+    /** Entête à afficher directement dans l'application */
+    public entete="<h1 class=\"maclasse-h1 maclasse-titreApplication\">Application de gestion de ma classe</h1>";
+
+    /** Constructeur pour injection des dépendances. */
+    public constructor(private contexteService: ContexteService) { super(); }
+
+    /** Au chargement du composant */
+    public ngOnInit(): void {
+
+        // Au chargement des données, récupéation de  la liste des élèves
+        const sub = this.contexteService.obtenirUnObservableDuChargementDesDonneesDeClasse().pipe(
+            tap(donnees => {
+                if (donnees && donnees.enteteEdition) {
+                    this.entete=donnees.enteteEdition;
+                }
+            })
+        ).subscribe();
+        super.declarerSouscription(sub);
+    }
 }
