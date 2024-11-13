2a8f5df50b7f652b097bb0840dea3bfbc991dcc2
Guillaume TALBOT <talbotgui@gmail.com>
2024-11-13 22:29:15 +0100

    Ajout du serveur local disponible en téléchargement

---
+++ zip/demarrerServeurLocal.cmd
@@ -0,0 +1,3 @@
+@rem outil utilisé disponible ici : https://github.com/TheWaWaR/simple-http-server
+server.exe -i
+echo "L'application locale est disponible à l'adresse http://localhost:8000/outilsDeClasse/
